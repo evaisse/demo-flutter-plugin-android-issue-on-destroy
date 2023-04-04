946e6e59b725022956e9ab8742e9ba753aff6f18
Emmanuel VAÏSSE <emmanuel.vaisse@boursorama.fr>
2023-04-04 15:54:33 +0200

    feat: add lifecycle events

--- android/src/main/kotlin/com/example/hellodestroy/HellodestroyPlugin.kt
+++ android/src/main/kotlin/com/example/hellodestroy/HellodestroyPlugin.kt
@@ -1,20 +1,41 @@
 package com.example.hellodestroy
 
+import android.util.Log
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+private const val TAG = "onDestroyIssue"
+
 /** HellodestroyPlugin */
-class HellodestroyPlugin: FlutterPlugin, MethodCallHandler {
+class HellodestroyPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    Log.d(TAG, "onAttachedToActivity")
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    Log.d(TAG, "onDetachedFromActivityForConfigChanges")
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    Log.d(TAG, "onReattachedToActivityForConfigChanges")
+  }
+
+  override fun onDetachedFromActivity() {
+    Log.d(TAG, "onDetachedFromActivity")
+  }
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hellodestroy")
--- example/android/app/src/main/kotlin/com/example/hellodestroy_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/hellodestroy_example/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.hellodestroy_example
 
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
+
+private const val TAG = "onDestroyIssue"
+
 class MainActivity: FlutterActivity() {
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy from MainActivity: FlutterActivity")
+        super.onDestroy()
+    }
 }
